2612fd4bb5a1d0a22f2184d7e00fe99a42da5b49
Rupali <rupaligajare507@gmail.com>
2023-05-20 21:52:12 +0530

    Update atm.java
    change in comment

--- atm.java
+++ atm.java
@@ -1,6 +1,8 @@
 import java.util.*;
 class Atm {
     public static void main(String[] args) {
+
+        //
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter username:");
         String name=sc.next();
